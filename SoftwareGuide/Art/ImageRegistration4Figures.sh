092d67ed544dc825c203af9ee9ceb46ae5fac06a
Luis Ibanez <luis.ibanez@kitware.com>
2005-06-11 14:14:19 -0400

    ENH: Cleaned up in order to make only one run of the registration,      and to generate the plot of translation at two zoom factors.

--- SoftwareGuide/Art/ImageRegistration4Figures.sh
+++ SoftwareGuide/Art/ImageRegistration4Figures.sh
@@ -3,15 +3,13 @@
 EXAMPLESBINDIR="/home/ibanez/bin/ITKGcc3.3/bin"
 DATADIR="/home/ibanez/src/Insight/Examples/Data"
 
-NUMBEROFBINS="30"
-
-OUTPUTDATAFILE="ImageRegistration4Output$NUMBEROFBINS.txt"
-OUTPUTDATAFILECLEANED="ImageRegistration4OutputCleaned$NUMBEROFBINS.txt"
+OUTPUTDATAFILE="ImageRegistration4Output.txt"
+OUTPUTDATAFILECLEANED="ImageRegistration4OutputCleaned.txt"
 
 #
 # Execute the example and capture the output.
 #
-$EXAMPLESBINDIR/ImageRegistration4 $DATADIR/BrainT1SliceBorder20.png $DATADIR/BrainProtonDensitySliceShifted13x17y.png ImageRegistration4Output.png 0 ImageRegistration4CheckerBoardAfter.png ImageRegistration4CheckerBoardBefore.png $NUMBEROFBINS | tee $OUTPUTDATAFILE
+$EXAMPLESBINDIR/ImageRegistration4 $DATADIR/BrainT1SliceBorder20.png $DATADIR/BrainProtonDensitySliceShifted13x17y.png ImageRegistration4Output.png 0 ImageRegistration4CheckerBoardAfter.png ImageRegistration4CheckerBoardBefore.png | tee $OUTPUTDATAFILE
 #
 # Take the first 200 lines, and remove the characters "["  "]"  ","
 #
@@ -20,4 +18,4 @@ head -n 200 $OUTPUTDATAFILE | sed "s/\]/ /g"  | sed "s/\,/ /g"  | sed "s/\[/ /g"
 #
 # Take the metric and translation data and generate plots with GNUPlot
 #
-#gnuplot ImageRegistration4Figures.gnup
+gnuplot ImageRegistration4Figures.gnup
