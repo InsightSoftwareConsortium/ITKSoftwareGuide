ec1b9870bff5ea89923a338e29328eefce33a638
Karthik Krishnan <karthik.krishnan@kitware.com>
2005-06-21 10:30:42 -0400

    BUG: correct inputs order

--- SoftwareGuide/Art/MultiResImageRegistration1Figures.sh
+++ SoftwareGuide/Art/MultiResImageRegistration1Figures.sh
@@ -6,7 +6,7 @@ DATADIR="/home/karthik/work/ITK/src/Insight/Nightly/Examples/Data"
 #
 # Execute the example and capture the output.
 #
-$EXAMPLESBINDIR/MultiResImageRegistration1 $DATADIR/BrainProtonDensitySliceShifted13x17y.png $DATADIR/BrainT1SliceBorder20.png MultiResImageRegistration1Output.png | tee MultiResImageRegistration1Output.txt
+$EXAMPLESBINDIR/MultiResImageRegistration1 $DATADIR/BrainT1SliceBorder20.png $DATADIR/BrainProtonDensitySliceShifted13x17y.png MultiResImageRegistration1Output.png | tee MultiResImageRegistration1Output.txt
 
 #
 # Take the first 6 lines, and remove the characters "["  "]"  ","
