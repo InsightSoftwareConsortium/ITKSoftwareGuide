e3c13b2ffc234f2376574b8eea4888b98b818607
Luis Ibanez <luis.ibanez@kitware.com>
2005-06-23 11:38:18 -0400

    ENH: Fixing the input image to be of the same modality.

--- SoftwareGuide/Art/ImageRegistration1Figures.sh
+++ SoftwareGuide/Art/ImageRegistration1Figures.sh
@@ -6,12 +6,12 @@ DATADIR="/home/ibanez/src/Insight/Examples/Data"
 #
 # Execute the example and capture the output.
 #
-$EXAMPLESBINDIR/ImageRegistration1 $DATADIR/BrainT1SliceBorder20.png $DATADIR/BrainProtonDensitySliceShifted13x17y.png ImageRegistration1Output.png | tee ImageRegistration1Output.txt
+$EXAMPLESBINDIR/ImageRegistration1 $DATADIR/BrainProtonDensitySliceBorder20.png $DATADIR/BrainProtonDensitySliceShifted13x17y.png ImageRegistration1Output.png | tee ImageRegistration1Output.txt
 
 #
-# Take the first 200 lines, and remove the characters "["  "]"  ","
+# Take the first N lines, and remove the characters "["  "]"  ","
 #
-head -n 200 ImageRegistration1Output.txt | sed "s/\]/ /g"  | sed "s/\,/ /g"  | sed "s/\[/ /g"  > ImageRegistration1OutputCleaned.txt
+head -n 15 ImageRegistration1Output.txt | sed "s/\]/ /g"  | sed "s/\,/ /g"  | sed "s/\=/ /g" | sed "s/\:/ /" | sed "s/\[/ /g"  > ImageRegistration1OutputCleaned.txt
 
 #
 # Take the metric and translation data and generate plots with GNUPlot
