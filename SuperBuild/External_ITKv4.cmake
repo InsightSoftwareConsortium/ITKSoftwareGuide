e531712d23e72f7c1eb05eda42aae0c9c82fe4be
Hans Johnson <hans-johnson@uiowa.edu>
2013-05-16 09:55:37 -0500

    COMP: Temporary point to private ITK until it is incorporated.

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -109,8 +109,11 @@ if(NOT ( DEFINED "${extProjName}_DIR" OR ( DEFINED "USE_SYSTEM_${extProjName}" A
       ${${proj}_FFTWD_ARGS}
     )
   ### --- End Project specific additions
-  set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git)
-  set(${proj}_GIT_TAG 05eff7a29c1633a5462a094884646a66c0bc3410)
+#set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git)
+#set(${proj}_GIT_TAG 05eff7a29c1633a5462a094884646a66c0bc3410)
+
+  set(${proj}_REPOSITORY ${git_protocol}://github.com/hjmjohnson/ITK.git)
+  set(${proj}_GIT_TAG SoftwareGuideLineLength)
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
     GIT_TAG ${${proj}_GIT_TAG}
