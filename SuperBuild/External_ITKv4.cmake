1c3a7f27922c9c096bb586bbb8039a86b0ac2cc9
Matt McCormick <matt.mccormick@kitware.com>
2014-12-18 19:52:54 -0500

    ENH: Bump ITK ExternalProject to v4.7.0.
    Change-Id: I6fd75404f5c26a63b3fb4145d4d96c7f90e30ae1

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -103,8 +103,7 @@ if(NOT ( DEFINED "${extProjName}_DIR" OR ( DEFINED "USE_SYSTEM_${extProjName}" A
     )
   ### --- End Project specific additions
   set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git)
-  # ITK release branch 2014-12-03 Bring closer to upcoming 4.7
-  set(${proj}_GIT_TAG 1dea442d456c9dd94784957f727a7fe9e8f83c47)
+  set(${proj}_GIT_TAG "v4.7.0")
 
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
