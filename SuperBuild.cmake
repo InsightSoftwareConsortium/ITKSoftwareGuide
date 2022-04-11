4f827f11f2dd5ed1a77d1861de5dda4dd8afc109
Dženan Zukić <dzenan.zukic@kitware.com>
2022-04-11 15:44:22 -0400

    ENH: Make HTTP protocol the default in SuperBuild.cmake

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -13,7 +13,7 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/Common.cmake)
 #-----------------------------------------------------------------------------
 # Git protocole option
 #-----------------------------------------------------------------------------
-option(${CMAKE_PROJECT_NAME}_USE_GIT_PROTOCOL "If behind a firewall turn this off to use http instead." ON)
+option(${CMAKE_PROJECT_NAME}_USE_GIT_PROTOCOL "If behind a firewall turn this off to use http instead." OFF)
 set(git_protocol "git")
 if(NOT ${CMAKE_PROJECT_NAME}_USE_GIT_PROTOCOL)
   set(git_protocol "http")
