245ebc7fd1ef7c34fe9a77a9d2b9a05a88f6ccbf
Hans Johnson <hans-johnson@uiowa.edu>
2026-04-21 06:15:08 -0500

    COMP: Bump ITK to v5.4.6
    Update the SuperBuild's pinned ITK tag from v5.4.4 to v5.4.6 so the
    ITKSoftwareGuide's code examples build against the 5.4.6 release.
    
    Release tracking: InsightSoftwareConsortium/ITK#6092

--- SuperBuild/External_ITK.cmake
+++ SuperBuild/External_ITK.cmake
@@ -104,8 +104,8 @@ if(NOT ( DEFINED "${extProjName}_DIR" OR ( DEFINED "USE_SYSTEM_${extProjName}" A
   ### --- End Project specific additions
   set(${proj}_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITK.git)
   if("${${proj}_GIT_TAG}" STREQUAL "")
-    # ITK release branch 2025-06-11
-    set(${proj}_GIT_TAG "v5.4.4")
+    # ITK release branch 2026-04-20
+    set(${proj}_GIT_TAG "v5.4.6")
   endif()
 
   ExternalProject_Add(${proj}
